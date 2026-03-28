cfdfb72e80271a210818c42c9b68a572c2b48f86
epiphonesgcustom <61947223+epiphonesgcustom@users.noreply.github.com>
2026-03-29 00:36:50 +0200

    Update service worker cache version to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const CACHE = 'podcast-gen-v1';
+// Increment this version number every time you update the app files.
+// This forces the service worker to fetch fresh versions from the server.
+const CACHE = 'podcast-gen-v3';
+
 const SHELL = [
   '/AIPodcast/podcast-generator.html',
   '/AIPodcast/manifest.json',
@@ -8,15 +11,19 @@ const SHELL = [
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then(c => c.addAll(SHELL))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
   );
 });
 
